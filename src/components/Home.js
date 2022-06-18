6ad70db1d0cab732035a829c511d7391632a7391
gefgu <gefgu@hotmail.com>
2022-06-18 09:50:38 -0300

    Refator hero image fetching into get popular images and top one movie image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,34 @@ import HeroSection from "./HeroSection";
 export default function Home() {
   const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 
+  const [popularMovies, setPopularMovies] = useState(null);
   const [heroImage, setHeroImage] = useState(null);
 
-  const getTopOneMovieImage = async () => {
+  const getPopularMovies = async () => {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_API_KEY}&language=en-US&page=1`
     );
     const dataListing = await response.json();
-    const topOneMovie = dataListing.results[0];
+    setPopularMovies(dataListing);
+  };
+
+  const getTopOneMovieImage = async () => {
+    const topOneMovie = popularMovies.results[0];
     const imagePath = `https://image.tmdb.org/t/p/original/${topOneMovie.backdrop_path}`;
     setHeroImage(imagePath);
   };
 
   useEffect(() => {
-    getTopOneMovieImage();
+    getPopularMovies();
   }, []);
+
+  useEffect(() => {
+    getTopOneMovieImage();
+  }, [popularMovies]);
 
   return (
     <main>
-      <HeroSection heroImage={heroImage}/>
+      <HeroSection heroImage={heroImage} />
       <div className="h-screen bg-stone-900 p-8">
         <section>
           <h3 className="text-white">Popular Movies</h3>

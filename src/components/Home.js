73bd317cc50afd303d061b41f1c1cc0be64cd3aa
gefgu <gefgu@hotmail.com>
2022-06-19 15:23:07 -0300

    Fix wrong titlte

--- src/components/Home.js
+++ src/components/Home.js
@@ -62,7 +62,7 @@ export default function Home() {
         )}
         {topRatedMovies && (
           <PostersListing
-            listingTitle="Popular Movies"
+            listingTitle="Top Rated Movies"
             posters={getMoviesPosters(topRatedMovies).slice(0, moviesInDisplay)}
           />
         )}

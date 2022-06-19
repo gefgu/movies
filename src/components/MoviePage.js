6abc6a3c7ac3fe4ebbeb50f081ee3419acd900bb
gefgu <gefgu@hotmail.com>
2022-06-19 20:55:33 -0300

    Add video of movie in movie page

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { convertMinutesIntoHoursAndMinutes } from "../helpers";
+import { getMoviePoster, convertMinutesIntoHoursAndMinutes } from "../helpers";
 
 export default function MoviePage() {
   const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
@@ -8,6 +8,7 @@ export default function MoviePage() {
   // 338953 - Fantastic Beasts 3 id
 
   const [movieDetails, setMovieDetails] = useState(null);
+  const [movieVideos, setMovieVideos] = useState(null);
 
   const getMovieDetails = async () => {
     const response = await fetch(
@@ -18,13 +19,23 @@ export default function MoviePage() {
     setMovieDetails(data);
   };
 
+  const getMovieVideos = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${MOVIE_API_KEY}&language=en-US`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    setMovieVideos(data.results);
+  };
+
   useEffect(() => {
     getMovieDetails();
+    getMovieVideos();
   }, []);
 
   return (
     movieDetails && (
-      <section className="p-4 bg-stone-800 text-white">
+      <section className="p-4 bg-stone-800 text-white xl:px-64">
         <h2 className="text-3xl font-bold mt-8 mb-2">
           {movieDetails.original_title}
         </h2>
@@ -36,6 +47,23 @@ export default function MoviePage() {
           }h ${
             convertMinutesIntoHoursAndMinutes(movieDetails.runtime).minutes
           }m`}</span>
+        </div>
+        <div className="py-8 flex gap-8">
+          <img
+            src={getMoviePoster(movieDetails)}
+            alt="Movie Poster"
+            className="max-h-96"
+          />
+          {movieVideos && (
+            <iframe
+              src={`https://www.youtube.com/embed/${movieVideos[0].key}`}
+              frameborder="0"
+              allow="autoplay; encrypted-media"
+              allowfullscreen
+              title="video"
+              className="w-full max-w-3xl"
+            />
+          )}
         </div>
       </section>
     )

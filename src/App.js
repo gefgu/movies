7130997df0d38681f301be62158fbb50e49a2954
gefgu <gefgu@hotmail.com>
2022-06-19 16:33:54 -0300

    Add routing to movie page with id param

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import MoviePage from "./components/MoviePage";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MoviePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

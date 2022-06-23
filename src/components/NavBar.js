a5cec6b9288c5e9cf727d61ebd0868857144102d
gefgu <gefgu@hotmail.com>
2022-06-23 19:36:28 -0300

    Add navbar search navigation to search page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function NavBar({ signInUser, signOutUser, user }) {
+  const navigate = useNavigate();
+  const searchBar = useRef();
+
+  const search = (e) => {
+    e.preventDefault();
+    const searchQuery = searchBar.current.value.replaceAll(" ", "+");
+    navigate(`search/${searchQuery}`);
+  };
+
   return (
     <nav className="flex bg-stone-900 text-white px-6 py-1 items-center h-16 gap-2 sm:gap-4 w-full xl:px-64 2xl:px-96">
       <button className="p-2 rounded text-white hover:bg-stone-800 active:bg-stone-700">
@@ -24,12 +34,16 @@ export default function NavBar({ signInUser, signOutUser, user }) {
       </Link>
       <form className="flex flex-1 items-center gap-4 justify-center">
         <input
+          ref={searchBar}
           name="search"
           type="text"
           className="border-2 text-black flex-1 py-1 px-2 hidden sm:block"
           placeholder="Search Movies"
         />
-        <button className="p-2 rounded hover:bg-stone-800 active:bg-stone-700">
+        <button
+          onClick={search}
+          className="p-2 rounded hover:bg-stone-800 active:bg-stone-700"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
